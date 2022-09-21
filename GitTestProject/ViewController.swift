d0b597316f299c7a2de93f9a13414e2257877d22
Dorukhan Demir <cryptodorko@gmail.com>
2022-09-21 13:06:50 +0300

    commit 1

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Dorukhan Demir on 21.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("git")
+        print("hello")
+        print("mello")
+        print("hi")
+        print("what's up")
+    }
+
+
+}
+
